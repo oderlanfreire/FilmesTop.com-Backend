0bc752469d5887dcdcc810bb8d41b0be4c787c60
Oderlan <oderlanfreire@gmail.com>
2024-09-14 08:42:26 -0300

    Dependency adicionado

--- main.py
+++ main.py
@@ -2,19 +2,12 @@ from fastapi import FastAPI
 from user.routers import user_router
 from models.userModel import User
 
-from database.database import SessionLocal, engine, Base
+from database.database import engine, Base
 
 Base.metadata.create_all(engine)
 
 
 app = FastAPI()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 @app.get("/")
 def read_root():

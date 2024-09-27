c2d704f5d0b458bd49755951692a58251bb3469d
Oderlan <oderlanfreire@gmail.com>
2024-09-27 17:06:54 -0300

    ajustes concluidos

--- models/User/userModel.py
+++ models/User/userModel.py
@@ -3,10 +3,12 @@ from sqlalchemy import  Column, VARCHAR, JSON
 import phonenumbers
 from database.database import Base
 
+def gerar_uuid():
+    return str(uuid.uuid4())
 class User(Base):
     __tablename__ = "usuario"
 
-    id = Column(VARCHAR(36), primary_key=True, default=str(uuid.uuid4()))
+    id = Column(VARCHAR(36), primary_key=True, default=gerar_uuid)
     nome = Column(VARCHAR(50), nullable= False)
     email = Column(VARCHAR(50), nullable= False)
     telefone = Column(VARCHAR(50), nullable= False)
@@ -17,5 +19,4 @@ class User(Base):
         if self.telefone:
             tel = phonenumbers.parse(self.telefone, None)
             return phonenumbers.format_number(tel, phonenumbers.PhoneNumberFormat.E164)
-
 
